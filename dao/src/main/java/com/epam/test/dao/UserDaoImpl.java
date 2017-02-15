c58f7fe93fa01bd58fa67599f2dd15d42523bcdf
VladSafronov <artem.safronov923@gmail.com>
2017-02-16 00:59:03 +0300

    Complete dao implementation(with tests)

--- dao/src/main/java/com/epam/test/dao/UserDaoImpl.java
+++ dao/src/main/java/com/epam/test/dao/UserDaoImpl.java
@@ -1,17 +1,22 @@
 package com.epam.test.dao;
 
 import com.epam.test.model.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.List;
 import java.util.Map;
 
@@ -37,25 +42,42 @@ public class UserDaoImpl implements UserDao {
     @Override
     public User getUserById(Integer userId) {
         SqlParameterSource namedParameters = new MapSqlParameterSource("p_user_id", userId);
-        User user = namedParameterJdbcTemplate.queryForObject(
-                "select user_id, login, password, description from app_user" +
-                " where user_id = :p_user_id", namedParameters, new UserRowMapper());
+        User user = null;
+        try {
+             user = namedParameterJdbcTemplate.queryForObject(
+                    "select user_id, login, password, description from app_user" +
+                            " where user_id = :p_user_id", namedParameters, new UserRowMapper());
+        }catch(EmptyResultDataAccessException e){
+
+        }
         return user;
     }
 
     @Override
     public Integer addUser(User user) {
-        return null;
+        SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(user);
+        String addUserSql = "INSERT INTO app_user (login,password,description) " +
+                            "VALUES (:Login,:Password,:Description)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        namedParameterJdbcTemplate.update(addUserSql, namedParameters, keyHolder);
+        return keyHolder.getKey().intValue();
     }
 
     @Override
     public void updateUser(User user) {
-
+        SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(user);
+        String updateUserSql = "UPDATE app_user SET " +
+                "login=:login,password=:password,description=:description " +
+                "WHERE user_id=:userId";
+        namedParameterJdbcTemplate.update(updateUserSql,namedParameters);
     }
 
     @Override
     public void deleteUser(Integer userId) {
-
+        Object[] params = {userId};
+        int[] types = {Types.BIGINT};
+        String deleteUserByIdSql = "DELETE FROM app_user WHERE user_id=?";
+        jdbcTemplate.update(deleteUserByIdSql,params,types);
     }
 
     private class UserRowMapper implements RowMapper<User> {
--- dao/src/test/java/com/epam/test/dao/UserDaoImplTest.java
+++ dao/src/test/java/com/epam/test/dao/UserDaoImplTest.java
@@ -2,15 +2,15 @@ package com.epam.test.dao;
 
 
 import com.epam.test.model.User;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.List;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:test-spring-dao.xml"})
@@ -28,20 +28,50 @@ public class UserDaoImplTest{
     @Test
     public void getUserByIdTest() throws Exception {
 
-        User user = userDao.getUserById(1);
+        User user = userDao.getUserById(2);
         assertNotNull(user);
-        assertEquals("userLogin1",user.getLogin());
+        assertEquals("userLogin2",user.getLogin());
+        user = userDao.getUserById(10);
+        assertEquals(null,user);
     }
     @Test
     public void addUserTest() throws Exception{
+        User user = new User(
+                3,
+                "TestUser3",
+                "userPassword",
+                "third test user"
+        );
+        Integer id = userDao.addUser(user);
+        User userFromDb = userDao.getUserById(user.getUserId());
+        assertEquals(user,userFromDb);
+        assertEquals(new Integer(3),id);
+        userDao.deleteUser(3);
+    }
 
+    @Test
+    public void updateUser() throws Exception{
+        User user = new User(
+                1,
+                "UpdatedUser",
+                "UpdatedPassword",
+                "UpdatedDescription");
+        User oldUser = userDao.getUserById(1);
+        assertNotEquals(user,oldUser);
+        userDao.updateUser(user);
+        User newUser = userDao.getUserById(1);
+        assertEquals(user,newUser);
     }
     @Test
-    public void updateUser(User user) throws Exception{
-
-    }
-    @Test
-    public void deleteUser(Integer userId) throws Exception{
+    public void deleteUser() throws Exception{
+        List<User> users = userDao.getAllUsers();
+        Integer sizeBeforeDelete = users.size();
+        User user = userDao.getUserById(1);
+        userDao.deleteUser(1);
+        users = userDao.getAllUsers();
+        Integer sizeAfterDelete = users.size();
+        assertEquals(1,sizeBeforeDelete-sizeAfterDelete);
+        assertEquals(null,userDao.getUserById(1));
 
     }
 
